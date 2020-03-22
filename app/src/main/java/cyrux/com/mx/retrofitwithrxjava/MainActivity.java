5a148953e1600133e0a3928a94af0234e4393553
Cyrux <cyl2ux@gmail.com>
2020-03-21 20:34:36 -0400

    Populate RecyclerView

--- app/src/main/java/cyrux/com/mx/retrofitwithrxjava/MainActivity.java
+++ app/src/main/java/cyrux/com/mx/retrofitwithrxjava/MainActivity.java
@@ -2,12 +2,20 @@ package cyrux.com.mx.retrofitwithrxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvRepositories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvRepositories = findViewById(R.id.rvRepositories);
+        rvRepositories.setLayoutManager(new LinearLayoutManager(this));
+        rvRepositories.setAdapter(new RepositoriesAdapter(Repository.getRepositories()));
     }
 }
